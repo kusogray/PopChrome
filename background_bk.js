ad92a03199e0e4c73d19689c6bb913a0bad2d292
kusogray <kusogray@hotmail.com>
2015-05-01 21:42:29 +0800

    test git hub commit

--- background_bk.js
+++ background_bk.js
@@ -1,3 +1,5 @@
+﻿// test git hub commit
+
   $(function() {
   //hljs.initHighlightingOnLoad();
   // $("body").prepend("<img height=\"700\" width=\"700\" src='http://i.imgur.com/SApd3wJ.jpg'  ></img>");
