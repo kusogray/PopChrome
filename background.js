63fe9a208c96af0015d5c6db022e04eb3270607e
kusogray <kusogray1@gmail.com>
2015-05-27 22:36:35 +0800

    update

--- background.js
+++ background.js
@@ -1,99 +1,28 @@
-function loadjscssfile(filename, filetype) {
-	if (filetype == "js") { //if filename is a external JavaScript file
-		var fileref = document.createElement('script')
-			fileref.setAttribute("type", "text/javascript")
-			fileref.setAttribute("src", filename)
-	} else if (filetype == "css") { //if filename is an external CSS file
-		var fileref = document.createElement("link")
-			fileref.setAttribute("rel", "stylesheet")
-			fileref.setAttribute("type", "text/css")
-			fileref.setAttribute("href", filename)
-	}
-	if (typeof fileref != "undefined")
-		document.getElementsByTagName("head")[0].appendChild(fileref)
-}
-
-$(function () {
-	//hljs.initHighlightingOnLoad();
-	// $("body").prepend("<img height=\"700\" width=\"700\" src='http://i.imgur.com/SApd3wJ.jpg'  ></img>");
-
-	//loadjscssfile("css/jquery-ui.css", "css") ////dynamically load and add this .css file
-	//loadjscssfile("js/jquery-ui.js", "js") //dynamically load "javascript.php" as a JavaScript file
-
-	$("body").prepend("<div id=\"danmu\" </div>");
-
-	$("body").prepend("<div id=\"danmu_dialog\" title=\"彈幕視窗\">  <input type=\"value\" id=\"danmuTextId\" value=\"豆豆豆鬥喔!!!!!\"> </input> </div>");
-
-	$("#danmu_dialog").dialog();
-	var tmpTxt = $("#danmuTextId").val();
-	$("#danmuTextId").keypress(function (e) {
-		if (e.which == 13) {
-			var tmpText = $("#danmuTextId").val();
-			var a_danmu2 = {
-				"text" : "豆喔!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-				"color" : "red",
-				"size" : "1",
-				"position" : "0",
-				"time" : 1,
-				"isnew" : " "
-			};
-			console.log(tmpText);
-			$('#danmu').danmu('danmu_resume');
-			$('#danmu').danmu("add_danmu", a_danmu2);
-		}
-	});
-
-	//console.log($('.t_fsz'));
-	/*  $("body").click(function(){
-	//var position = $(".drive-viewer-video-player" ).position();
-	console.log($('.drive-viewer-video-player'));
-	console.log($('.t_fsz'));
-	//alert("left: " + position.left + ", top: " + position.top );
-	}	); */
-
-	//$("#gc-container").remove();
-
-	//window.open("popup.html");
-	console.log("Danmu Start");
-	var a_danmu = {
-		"text" : "豆喔!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-		"color" : "red",
-		"size" : "1",
-		"position" : "0",
-		"time" : 1,
-		"isnew" : " "
-	};
-
-	var a_danmu2 = {
-		"text" : "豆豆豆鬥喔!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-		"color" : "red",
-		"size" : "1",
-		"position" : "0",
-		"time" : 1,
-		"isnew" : " "
-	};
-
-	console.log("Danmu init.");
-	$("#danmu").danmu({
-		// left:$("#danmuarea").offset().left,
-		// top:$("#danmuarea").offset().top,
-		// height: 445,
-		//    width: 800,
-		left : 0,
-		top : 0,
-		height : "100%",
-		width : "100%",
-		speed : 30000,
-		zindex : 1000,
-		opacity : 1,
-		font_size_small : 16,
-		font_size_big : 24,
-		top_botton_danmu_time : 6000
-	});
-
-	$('#danmu').danmu('danmu_resume');
-	$('#danmu').danmu("add_danmu", a_danmu);
-	$('#danmu').danmu("add_danmu", a_danmu2);
-
-	console.log("Danmu Finish");
+// Set up context menu at install time.
+chrome.runtime.onInstalled.addListener(function() {
+  var context = "all";
+  var title = "DanMu GO!";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "id": "context" + context});  
+										 
+var child1 = chrome.contextMenus.create({"title": "開啟彈幕視窗", "parentId": id, "onclick": onClickHandler, "contexts": [context]});
+var child2 = chrome.contextMenus.create({"title": "開啟彈幕", "parentId": id, "onclick": onClickHandler, "contexts": [context]});									 
+var child3 = chrome.contextMenus.create({"title": "關閉彈幕", "parentId": id, "onclick": onClickHandler, "contexts": [context]});									 
+										 
 });
+
+// add click event
+//chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+  /*var sText = info.selectionText;
+  var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);  
+  window.open(url, '_blank');*/
+  alert(tab.id);
+  chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(response) {
+   alert("test");
+  });
+};
+
+
--- content_script.js
+++ content_script.js
@@ -5,6 +5,8 @@
 1.0.2		2015/05/17	kusogray	fire on enter key pressed
 1.0.2.1		2015/05/17	kusogray	send danmu as a function
 1.0.3		2015/05/17	kusogray	send to firebase
+1.0.4		2015/05/24	kusogray	send / receive msg from background.js to content_script.js
+1.0.5		2015/05/24	kusogray	get json from firebase
 
 
  */
@@ -12,50 +14,140 @@
 //1.0.1
 var myFirebaseRef = new Firebase("https://popchrome.firebaseio.com/");
 
+// 1.0.4
+chrome.runtime.onMessage.addListener(
+	function (request, sender, sendResponse) {
+	alert("test3");
+	console.log(sender.tab ?
+		"from a content script:" + sender.tab.url :
+		"from the extension");
+	// if (request.greeting == "hello")
+	sendResponse({
+		farewell : "goodbye"
+	});
+});
+
+var htmlTagFlag = false;
+
 $(function () {
 
-	var rect = {};
-	//rect = getVideoPos();
-	document.body.addEventListener('click', function () {
+	/*jQuery(document).ready(function () {
+	jQuery('video').bind('contextmenu', function () {
+	return false;
+	});
+	});*/
 
-		//rect = getVideoPos();
+	/*$("h2").on('click', 'p.test', function() {
+		alert('you clicked a p.test element');
+	});*/
 
-		var time = $('#danmu').data("nowtime")
-			console.log("clicked!!" + time);
-		var a_danmu = {
-			"text" : "豆222喔!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-			"color" : "red",
-			"size" : "1",
-			"position" : "0",
-			"time" : time + 3,
-			"isnew" : " "
-		};
+	/*$("#container").bind("DOMNodeInserted",function(){
+		alert("child is appended");
+	});*/
 
-		//$('#danmu').danmu("add_danmu",a_danmu);
+	$(document).on('click', 'video', function() {
+		if (true) {
+			$('video').bind('contextmenu', function () {
 
+				event.preventDefault();
+				$("<div class='custom-popchrome-menu'>開啟彈幕視窗</div>")
+				.appendTo("body")
+				.css({
+					top : event.pageY + "px",
+					left : event.pageX + "px"
+				});
+				$(".custom-popchrome-menu").css("z-index", "1000");
+				$(".custom-popchrome-menu").css("position", "absolute");
+				$(".custom-popchrome-menu").css("position", "absolute");
+				$(".custom-popchrome-menu").css("background-color", "#C0C0C0");
+				//$(".custom-popchrome-menu").css("background-color","#C0C0C0");
+				$(".custom-popchrome-menu").css("border", "1px solid black");
+				$(".custom-popchrome-menu").css("padding", "2px");
+				$(".custom-popchrome-menu").css("height", "20");
+
+				return false; // video preventDefault
+
+
+				/* z-index:1000;
+				position: absolute;
+				background-color:#C0C0C0;
+				border: 1px solid black;
+				padding: 2px;*/
+			});
+		}
 	});
 
-	var htmlTag = " <select  name=\"color\" id=\"danMuUserColor\" > <option value=\"red\">紅色</option>   " +
-		"               <option value=\"white\">白色</option> " +
-		"               <option value=\"green\">綠色</option>                           " +
-		"               <option value=\"blue\">藍色</option>                            " +
-		"               <option value=\"yellow\">黃色</option>                          " +
-		"               </select>                                                       " +
-		"               <select name=\"size\" id=\"danMuUserTextSize\" >                        " +
-		"               <option value=\"1\">大文字</option>                             " +
-		"               <option value=\"0\">小文字</option>                             " +
-		"               </select>                                                       " +
-		"               <select name=\"position\" id=\"danMuUserPosition\"   >                   " +
-		"               <option value=\"0\">滾動</option>                               " +
-		"               <option value=\"1\">頂端</option>                               " +
-		"               <option value=\"2\">底端</option>                               " +
-		"               </select>                                                       " +
-		"           <input type=\"textarea\" id=\"danMuUserText\" max=300 />                     " +
-		"            <button type=\"button\" id=\"danMuUserBtn\" >送出</button>       ";
+	$(document).mousedown(function (event) {
+		if (event.which == 1) {
+			if (event.target.className == "custom-popchrome-menu") {
+				if (!htmlTagFlag) {
+					var htmlTag = " <select  name=\"color\" id=\"danMuUserColor\" > <option value=\"red\">紅色</option>   " +
+						"               <option value=\"white\">白色</option> " +
+						"               <option value=\"green\">綠色</option>                           " +
+						"               <option value=\"blue\">藍色</option>                            " +
+						"               <option value=\"yellow\">黃色</option>                          " +
+						"               </select>                                                       " +
+						"               <select name=\"size\" id=\"danMuUserTextSize\" >                        " +
+						"               <option value=\"1\">大文字</option>                             " +
+						"               <option value=\"0\">小文字</option>                             " +
+						"               </select>                                                       " +
+						"               <select name=\"position\" id=\"danMuUserPosition\"   >                   " +
+						"               <option value=\"0\">滾動</option>                               " +
+						"               <option value=\"1\">頂端</option>                               " +
+						"               <option value=\"2\">底端</option>                               " +
+						"               </select>                                                       " +
+						"           <input type=\"textarea\" id=\"danMuUserText\" max=300 />                     " +
+						"            <button type=\"button\" id=\"danMuUserBtn\" >送出</button>       ";
 
-	$("body").prepend("<div id=\"danmu\" </div>");
-	$("body").prepend("<div id=\"danmu_dialog\" title=\"彈幕視窗\">  " + htmlTag);
-	$("#danmu_dialog").dialog();
+					$("body").prepend("<div id=\"danmu\" </div>");
+					$("body").prepend("<div id=\"danmu_dialog\" title=\"彈幕視窗\">  " + htmlTag);
+					htmlTagFlag = true;
+				}
+
+				$("#danmu_dialog").dialog();
+			}
+		}
+
+		$(".custom-popchrome-menu").remove();
+	});
+
+
+
+	/*$.contextMenu({
+	selector: 'video',
+	callback: function(key, options) {
+	var m = "clicked: " + key;
+	window.console && console.log(m) || alert(m);
+	},
+	items: {
+	"edit": {name: "Edit", icon: "edit"},
+	"cut": {name: "Cut", icon: "cut"},
+	"copy": {name: "Copy", icon: "copy"},
+	"paste": {name: "Paste", icon: "paste"},
+	"delete": {name: "Delete", icon: "delete"},
+	"sep1": "---------",
+	"quit": {name: "Quit", icon: "quit"}
+	}
+	});*/
+
+	var video = $("video");
+
+	$('video').on('click', function (e) {
+		console.log('clicked hoho !!!!!!!');
+	})
+
+	var rect = {};
+	rect = getVideoPos();
+	/*document.body.addEventListener('click', function () {
+
+	//rect = getVideoPos();
+
+	var time = $('#danmu').data("nowtime")
+	console.log("clicked!!" + time);
+
+
+	});*/
+
 	console.log("Danmu Start");
 
 	/* myDataRef.on('child_added', function(snapshot) {
